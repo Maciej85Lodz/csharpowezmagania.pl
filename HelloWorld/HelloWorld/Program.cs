2ee62746581b43692a49c9dbf8926ff676a72aab
maciej.wolejszo <maciej.wolejszo@gmail.com>
2018-04-10 22:11:54 +0200

    Post - STRUKTURA PROGRAMU...-

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* powyżej przestrzeń nazw*/
+
+namespace HelloWorld /* deklaracja przestrzeni nazw*/
+{
+    class Program /*Klasa Programu */
+    {
+        static void Main(string[] args) /* Metoda wykonywana przez klasę PRogram*/
+        {
+
+            /*Instrukcja do wykonania przez metodę Main*/
+            Console.WriteLine("Hello World My name is Maciej");
+            Console.ReadKey();
+        }
+    }
+}
