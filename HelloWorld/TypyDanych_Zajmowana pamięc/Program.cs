f5814539491231d9fe39f71726ecd7191e5dffa8
maciej.wolejszo <maciej.wolejszo@gmail.com>
2018-04-10 22:41:52 +0200

    Post Typy Danych - SizeOf

---
+++ HelloWorld/TypyDanych_Zajmowana pamięc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypyDanych_Zajmowana_pamięc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rozmiar szukanej zmiennej typu to: {0}",sizeof(double)); //szukany typ danych to Double
+            Console.ReadLine();
+        }
+    }
+}
