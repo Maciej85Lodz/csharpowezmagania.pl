5295a83dc1f084de6e75bbde72a92190b7ecee2d
maciej.wolejszo <maciej.wolejszo@gmail.com>
2018-06-28 00:44:09 +0200

    csharpowezmagania.pl Metody w C# Wywoływanie Metod; Rekurencyjne wywoływanie metod

---
+++ Methods/Methods/Factorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Factorial
+    {
+        private int number;
+
+        public int ComputeFactorial(int liczba)
+        {
+            int score;
+            if (number == 1)
+                return 1;
+            else
+            {
+                score = ComputeFactorial(number - 1) * number;
+                return score;
+            }
+        }
+
+        public Factorial(int number)
+        {
+            this.number = number;
+        }
+
+        public Factorial()
+        {
+        }
+    }
+}
--- Methods/Methods/GreaterNumber.cs
+++ Methods/Methods/GreaterNumber.cs
@@ -20,4 +20,5 @@ namespace Methods
         }
 
     }
+
 }
