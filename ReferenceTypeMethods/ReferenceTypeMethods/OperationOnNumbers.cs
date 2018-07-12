4e89cbb0c956de9d3fc53f187e63426fda3c430e
maciej.wolejszo <maciej.wolejszo@gmail.com>
2018-07-12 18:38:24 +0200

    Reference Type methods

---
+++ ReferenceTypeMethods/ReferenceTypeMethods/OperationOnNumbers.cs
@@ -0,0 +1,14 @@
+﻿namespace ReferenceTypeMethods
+{
+    internal class OperationOnNumbers
+    {
+        public void ChangeQueue(ref int x, ref int y)
+        {
+            int transient;
+            transient = x; // assign the value of the variable x
+            x = y;  // assigning the y value in place of the x variable
+            y = transient; // assigning the value of x to the variable y
+        }
+
+    }
+}
---
+++ ReferenceTypeMethods/ReferenceTypeMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReferenceTypeMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperationOnNumbers onl = new OperationOnNumbers();
+            int a = 25;
+            int b = 50;
+            Console.WriteLine("Wartość a przed zamianą: {0}", a);
+            Console.WriteLine("Wartość b przed zamianą: {0}", b);
+            // Call the reverse order method
+            onl.ChangeQueue(ref a, ref b);
+            Console.WriteLine("Wartość a po zmianie: {0}", a);
+            Console.WriteLine("Wartość b po zmianie: {0}", b);
+            Console.ReadKey();
+            // The result of the program 
+            // The value before the conversion: 25 
+            // The value of b before conversion: 50 
+            // The value after the change: 25 
+            // The value of b after the change: 50
+
+        }
+    }
+}
