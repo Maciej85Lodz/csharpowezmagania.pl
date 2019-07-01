9d57362701e0cd6def788704f298b642e7cb32b4
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-07-01 22:51:08 +0200

    Namespaces with using

---
+++ NameSpaces/NameSpaces/NamespacesWithUsing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using first_name_space;
+using second_name_space;
+
+namespace NameSpaces
+{
+    class NamespacesWithUsing
+    {static void Main(string[] args)
+        {
+            namespace_1 p1 = new namespace_1();
+            namespace_2 p2 = new namespace_2();
+            p1.method();
+            p2.method();
+            Console.ReadKey();
+            // The result of the program
+            // // Inside the first namespace
+            // // Inside the second namespace
+        }
+    }
+}
