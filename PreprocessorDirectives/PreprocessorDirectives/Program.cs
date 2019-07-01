114e8be1c3dbe7df587f70b913a7baeb72c14a4c
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-07-01 23:17:23 +0200

    Preprocessor Directives

---
+++ PreprocessorDirectives/PreprocessorDirectives/Program.cs
@@ -0,0 +1,15 @@
+﻿#define MyVariable;
+using System;
+namespace Directive {
+    class Program {
+        static void Main(string[] args)
+        {
+            #if (MyVariable)
+                        Console.WriteLine("Variable is defined");
+            #else
+                        Console.WriteLine("Variable is not defined"); 
+            #endif
+                         Console.ReadKey(); 
+        }
+    }
+}
