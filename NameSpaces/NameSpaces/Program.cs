5e6f966795d087e18055c6f24648e5b94d493491
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-07-01 22:36:31 +0200

    Namepaces

---
+++ NameSpaces/NameSpaces/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NameSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new first_name_space.namespace_1();
+            var p2 = new second_name_space.namespace_2();
+            p1.method();
+            p2.method();
+            Console.ReadKey(); 
+            // The result of the program
+            // // Inside the first namespace
+            // // Inside the second namespace
+        }
+    }
+}
+
+namespace first_name_space
+{
+    class namespace_1
+    {
+        public void method()
+        {
+            Console.WriteLine("Inside first namespace");
+        }
+    }
+}
+
+namespace second_name_space
+{
+    class namespace_2
+    {
+        public void method()
+        {
+            Console.WriteLine("Inside second namespace");
+        }
+    }
+}
